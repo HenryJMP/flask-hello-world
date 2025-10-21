e5c0ff476cbb44090f26b478cf359973020a93fa
Enrique Jimenez-Moran Y Perez <a01635419@tec.mx>
2025-10-21 08:21:19 -0600

    Implement database connection in sensor route
    Added database connection and query execution in the sensor route.

--- api/index.py
+++ api/index.py
@@ -1,4 +1,14 @@
 from flask import Flask
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+# Fetch variables
+USER = os.getenv("user")
+PASSWORD = os.getenv("password")
+HOST = os.getenv("host")
+PORT = os.getenv("port")
+DBNAME = os.getenv("dbname")
 
 app = Flask(__name__)
 
@@ -12,8 +22,40 @@ def about():
 
 @app.route('/sensor')
 def sensor():
-    return 'Soy el sensor'
+    # Connect to the database
+    try:
+        connection = psycopg2.connect(
+            user=USER,
+            password=PASSWORD,
+            host=HOST,
+            port=PORT,
+            dbname=DBNAME
+        )
+        print("Connection successful!")
+        
+        # Create a cursor to execute SQL queries
+        cursor = connection.cursor()
+        
+        # Example query
+        cursor.execute("SELECT NOW();")
+        result = cursor.fetchone()
+        print("Current Time:", result)
+    
+        # Close the cursor and connection
+        cursor.close()
+        connection.close()
+        print("Connection closed.")
+        return f"Current Time:{result}"
+    
+    except Exception as e:
+        print(f"Failed to connect: {e}")'
 
 @app.route('/sesgar')
 def sesgar():
     return 'SesgarCLaCome'
+
+# Load environment variables from .env
+load_dotenv()
+
+
+
